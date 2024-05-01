a037ee1153e864e1cb52de5f0b5760cf29e5fada
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2024-05-01 16:23:37 +0200

    Initial commit

---
+++ my-terminal.js
@@ -0,0 +1,217 @@
+const font = 'Slant';
+
+figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
+figlet.preloadFonts([font], ready);
+
+const formatter = new Intl.ListFormat('en', {
+  style: 'long',
+  type: 'conjunction',
+});
+
+const directories = {
+    hobbies: [
+        '',
+        '<white>hobbies</white>',
+        '* <yellow>Korean Tv-Shows</yellow>',
+        '* <yellow>Mystery Novels</yellow>',
+        '* <yellow>Penguins</yellow>',
+        '* <yellow>Anime</yellow>',
+        ''
+    ],
+    projects: [
+        '',
+        '<white>Projects</white>',
+        [
+            ['BEAST CRIMES','https://www.beastcrimes.com/','Mystery PC game'],
+            ['Space Out Cat','https://www.spaceoutcat.com/', 'Self-care health app'],
+            ['Kakong','https://play.google.com/store/apps/details?id=com.linkthecoder.kakong', 'A korean learning app'],
+        ].map(([name, url, description = '']) => {
+            return `* <a href="${url}">${name}</a> &mdash; <white>${description}</white>`;
+        }),
+        ''
+    ].flat(),
+    skills: [
+        '',
+        '<white>coding languages</white>',
+
+        [
+            'HTML',
+            'CSS',
+            'JS/TS',
+            'JSX/TSX',
+            'Nani',
+            'Markdown',
+        ].map(lang => `* <yellow>${lang}</yellow>`),
+        '',
+        '<white>frameworks</white>',
+        [
+            'React',
+            'Next',
+            'Tailwind CSS',
+        ].map(lib => `* <green>${lib}</green>`),
+        '',
+        '<white>testing</white>',
+        [
+            'Jest',
+            'RTL',
+            'Playwright'
+        ].map(lib => `* <blue>${lib}</blue>`),
+        ''
+    ].flat()
+};
+
+const dirs = Object.keys(directories);
+
+const root = '~';
+let cwd = root;
+
+const user = 'linkthecoder';
+const server = 'desktop';
+
+function prompt() {
+    return `<white>Type <green>help</green> to see available commands.</white>\n` +
+           `<green>${user}@${server}</green>:<blue>${cwd}</blue>$ `;
+}
+
+function print_dirs() {
+     term.echo(dirs.map(dir => {
+         return `<blue class="directory">${dir}</blue>`;
+     }).join('\n'));
+}
+
+const commands = {
+    help() {
+        term.echo(`List of available commands: ${help}`);
+    },
+    ls(dir = null) {
+        if (dir) {
+            if (dir.startsWith('~/')) {
+                const path = dir.substring(2);
+                const dirs = path.split('/');
+                if (dirs.length > 1) {
+                    this.error('Invalid directory');
+                } else {
+                    const dir = dirs[0];
+                    this.echo(directories[dir].join('\n'));
+                }
+            } else if (cwd === root) {
+                if (dir in directories) {
+                    this.echo(directories[dir].join('\n'));
+                } else {
+                    this.error('Invalid directory');
+                }
+            } else if (dir === '..') {
+                print_dirs();
+            } else {
+                this.error('Invalid directory');
+            }
+        } else if (cwd === root) {
+           print_dirs();
+        } else {
+            const dir = cwd.substring(2);
+            this.echo(directories[dir].join('\n'));
+        }
+    },
+    cd(dir = null) {
+        if (dir === null || (dir === '..' && cwd !== root)) {
+            cwd = root;
+        } else if (dir.startsWith('~/') && dirs.includes(dir.substring(2))) {
+            cwd = dir;
+        } else if (dirs.includes(dir)) {
+            cwd = root + '/' + dir;
+        } else {
+            this.error('Wrong directory');
+        }
+    },
+    echo(...args) {
+        if (args.length > 0) {
+            term.echo(args.join(' '));
+        }
+    }
+};
+
+const command_list = ['clear'].concat(Object.keys(commands));
+const formatted_list = command_list.map(cmd => `<white class="command">${cmd}</white>`)
+const help = formatter.format(formatted_list);
+
+const re = new RegExp(`^\s*(${command_list.join('|')})(\s?.*)`);
+
+$.terminal.new_formatter([re, function(_, command, args) {
+    return `<white class="command">${command}</white><aquamarine>${args}</aquamarine>`;
+}]);
+
+$.terminal.xml_formatter.tags.blue = (attrs) => {
+    return `[[;#55F;;${attrs.class}]`;
+};
+$.terminal.xml_formatter.tags.green = (attrs) => {
+    return `[[;#44D544;]`;
+};
+
+const term = $('body').terminal(commands, {
+    greetings: false,
+    checkArity: false,
+    completion(string) {
+        const { name, rest } = $.terminal.parse_command(this.get_command());
+        if (['cd', 'ls'].includes(name)) {
+            if (rest.startsWith('~/')) {
+                return dirs.map(dir => `~/${dir}`);
+            }
+            if (cwd === root) {
+                return dirs;
+            }
+        }
+        return Object.keys(commands);
+    },
+    prompt
+});
+
+term.pause();
+
+term.on('click', '.command', function() {
+   const command = $(this).text();
+   term.exec(command, { typing: true, delay: 50 });
+});
+
+term.on('click', '.directory', function() {
+    const dir = $(this).text();
+    term.exec(`cd ~/${dir}`, { typing: true, delay: 50 });
+});
+
+function ready() {
+   const seed = rand(256);
+   term.echo(() => rainbow(render('Terminal'), seed))
+       .echo().resume();
+}
+
+function rainbow(string, seed) {
+    return lolcat.rainbow(function(char, color) {
+        char = $.terminal.escape_brackets(char);
+        return `[[;${hex(color)};]${char}]`;
+    }, string, seed).join('\n');
+}
+
+function rand(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function render(text) {
+    const cols = term.cols();
+    return trim(figlet.textSync(text, {
+        font: font,
+        width: cols,
+        whitespaceBreak: true
+    }));
+}
+
+function trim(str) {
+    return str.replace(/[\n\s]+$/, '');
+}
+
+function hex(color) {
+    return '#' + [color.red, color.green, color.blue].map(n => {
+        return n.toString(16).padStart(2, '0');
+    }).join('');
+}
+
+
+github('jcubic/jquery.terminal');
